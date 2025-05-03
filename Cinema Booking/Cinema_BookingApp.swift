286bae075dfae4d8edb7894803f020fd59e0982f
Sooryasanand <sooryanarayanan.com@gmail.com>
2025-05-03 16:42:47 +1000

    Initial Commit

---
+++ Cinema Booking/Cinema_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinema_BookingApp.swift
+//  Cinema Booking
+//
+//  Created by Soorya Narayanan Sanand on 3/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cinema_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinema Booking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cinema Booking
+//
+//  Created by Soorya Narayanan Sanand on 3/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
